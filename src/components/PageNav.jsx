bd6afbead14cfee033a42fd0c9a8d73f142f44b1
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2024-08-28 10:44:59 -0400

    setup app navigation

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -7,21 +7,16 @@ function PageNav() {
     <nav className={styles.nav}>
       <ul>
         <li>
-          <NavLink to={'/product'}>
+          <NavLink to={'/'}>
             <Logo />
           </NavLink>
         </li>
         <li>
-          <NavLink to={'/product'}>Product</NavLink>
+          <NavLink to={'product'}>Product</NavLink>
         </li>
 
         <li>
-          <NavLink to={'/pricing'}>Pricing</NavLink>
-        </li>
-        <li>
-          <NavLink to={'/login'} className={styles.ctaLink}>
-            Login
-          </NavLink>
+          <NavLink to={'pricing'}>Pricing</NavLink>
         </li>
       </ul>
     </nav>
