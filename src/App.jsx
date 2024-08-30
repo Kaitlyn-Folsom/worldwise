6ef82b9bb349017f34f2f201dc3c2853a53a074a
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2024-08-29 20:46:21 -0400

    setup json-server and render initial cities list from data

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
@@ -5,8 +6,32 @@ import Homepage from './pages/Homepage';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
+import CityList from './components/CityList';
 
+const BASE_URL = `http://localhost:9000`;
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+        setIsLoading(false);
+      } catch {
+        throw new Error('error fetching data');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  console.log(cities);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -14,8 +39,8 @@ function App() {
         <Route path={'product'} element={<Product />} />
         <Route path={'pricing'} element={<Pricing />} />
         <Route path={'app'} element={<AppLayout />}>
-          <Route index element={<p>cities</p>} />
-          <Route path={'cities'} element={<p>Cities</p>} />
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path={'cities'} element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path={'countries'} element={<p>countries</p>} />
           <Route path={'form'} element={<p>Form</p>} />
         </Route>
