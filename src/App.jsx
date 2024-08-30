31149ae7b4f467cd1eefb74437974c1e0b158709
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2024-08-30 19:38:25 -0400

    additional navigation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
@@ -8,6 +8,8 @@ import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
 import CityList from './components/CityList';
 import CountriesList from './components/CountriesList';
+import City from './components/City';
+import Form from './components/Form';
 
 const BASE_URL = `http://localhost:9000`;
 function App() {
@@ -40,10 +42,11 @@ function App() {
         <Route path={'product'} element={<Product />} />
         <Route path={'pricing'} element={<Pricing />} />
         <Route path={'app'} element={<AppLayout />}>
-          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route index element={<Navigate replace to='cities' />} />
           <Route path={'cities'} element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path={'cities/:id'} element={<City />} />
           <Route path={'countries'} element={<CountriesList cities={cities} isLoading={isLoading} />} />
-          <Route path={'form'} element={<p>Form</p>} />
+          <Route path={'form'} element={<Form />} />
         </Route>
         <Route path={'login'} element={<Login />} />
         <Route path={'*'} element={<PageNotFound />} />
