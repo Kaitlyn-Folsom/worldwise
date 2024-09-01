dacaab1171c3208e45d6c289fd407dc2e4c215ca
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2024-09-01 17:17:14 -0400

    setup cities provider for global state

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -3,8 +3,10 @@ import CityItem from './CityItem';
 import styles from './CityList.module.css';
 import Spinner from './Spinner';
 import Message from './Message';
+import { useCities } from '../contexts/CitiesContext';
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
   console.log('loading', isLoading);
   console.log(cities);
   if (isLoading) return <Spinner />;
--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -3,8 +3,10 @@ import CountryItem from './CountryItem';
 import styles from './CountryList.module.css';
 import Spinner from './Spinner';
 import Message from './Message';
+import { useCities } from '../contexts/CitiesContext';
 
-function CountriesList({ cities, isLoading }) {
+function CountriesList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
 
   if (!cities.length) return <Message message={'Add you first city by clicking a city on the map'} />;
