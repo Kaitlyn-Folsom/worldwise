19d1ea8efaf01a29692d29528c0aeb053a71469d
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2024-09-22 09:10:38 -0400

    setup auth context

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -5,18 +5,19 @@ import Logo from './Logo';
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to={'/'}>
-            <Logo />
-          </NavLink>
-        </li>
         <li>
           <NavLink to={'product'}>Product</NavLink>
         </li>
 
         <li>
           <NavLink to={'pricing'}>Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to='/login' className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
